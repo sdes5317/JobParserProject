57cd302c7171e2c0ba901912b99097b84404320e
ko5317 <sdes5317@yahoo.com.tw>
2020-11-13 09:27:30 +0800

    fix MySql ConnectionString is dynamic problem

--- JobWebApi/Repository/JobContext.cs
+++ JobWebApi/Repository/JobContext.cs
@@ -1,6 +1,9 @@
-﻿using JobWebApi.Repository.Entity;
+﻿using System;
+using System.Text.RegularExpressions;
+using JobWebApi.Repository.Entity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using MySql.Data.EntityFrameworkCore.Extensions;
 
 namespace JobWebApi.Repository
@@ -10,9 +13,17 @@ namespace JobWebApi.Repository
         private readonly string _connectionString;
         public DbSet<JobDetail> JobDetails { get; set; }
 
-        public JobContext(IConfiguration configuration)
+        public JobContext()
         {
-            _connectionString = configuration.GetConnectionString("MySql");
+            var rawConnectionString = Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb");
+            //We will get this
+            //Database=localdb;Data Source=127.0.0.1:50754;User Id=azure;Password=6#vWHD_$
+            //Need change to this 
+            //"Server=127.0.0.1; Port=50726; Database=localdb; Uid=azure; Pwd=6#vWHD_$; Character Set=utf8"
+            _connectionString = rawConnectionString
+                .Replace("Data Source=127.0.0.1:", "Port=")
+                .Replace("User Id", "Uid")
+                .Replace("Password", "Pwd") + ";Server=127.0.0.1; Character Set=utf8";
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
@@ -27,6 +38,21 @@ namespace JobWebApi.Repository
                 builder.HasIndex(jobDetail => jobDetail.CreationTime);
                 builder.Property(jobDetail => jobDetail.CreationTime);
             });
+        }
+
+        private string GetMySqlConnectionString()
+        {
+            var rawConnectionString = Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb") 
+                                      ?? throw new NullReferenceException("MYSQLCONNSTR_localdb Parameter is null !" +
+                                                                          "Please Check MySql In Azure is Open !");
+            //We will get this
+            //Database=localdb;Data Source=127.0.0.1:50754;User Id=azure;Password=6#vWHD_$
+            //Need change to this 
+            //"Server=127.0.0.1; Port=50726; Database=localdb; Uid=azure; Pwd=6#vWHD_$; Character Set=utf8"
+            return rawConnectionString
+                .Replace("Data Source=127.0.0.1:", "Port=")
+                .Replace("User Id", "Uid")
+                .Replace("Password", "Pwd") + ";Server=127.0.0.1; Character Set=utf8";
         }
     }
 }
